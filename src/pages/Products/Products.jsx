f7d79b9e4ee77a67c43ac6107ee4f065a094a161
jovanlekic12 <lekicjovan1212@gmail.com>
2025-02-11 23:28:54 +0100

    add data fetching

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
 function ProductsPage() {
+  const [products, setProducts] = useState();
+  const url = "https://www.course-api.com/react-store-products";
+  async function fetchProducts() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return <></>;
 }
 export default ProductsPage;
